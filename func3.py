974a8073659f1e3cfcb73aecc52f579e9585e0b9
snyang83 <sn01.yang@gmail.com>
2023-10-24 14:28:20 +0900

    클래스연습1

--- func3.py
+++ func3.py
@@ -69,3 +69,10 @@ print(list(range(1,32)))
 
 for i in range(10):
     print(i)
+
+
+print("----리스트 함축----")
+lst = list(range(1,11))
+print([i**2 for i in lst if i >5 ])
+fruits = ("apple", "oragne", "kiwi")
+print([len(i) for i in fruits])
