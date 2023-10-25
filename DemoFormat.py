421d152011109fb3b97868cdf0376915abcba794
snyang83 <sn01.yang@gmail.com>
2023-10-25 10:07:07 +0900

    파일 연습 완료

--- DemoFormat.py
+++ DemoFormat.py
@@ -28,10 +28,27 @@ f = open("demo.txt","wt", encoding="utf-8")
 f.write("첫번째\n두번째\n세번째\n")
 f.close()
 
+#기존 파일에 첨부 
+f = open("demo.txt", "a+", encoding="utf-8")
+f.write("다른 내용을 첨부\n")
+f.close()
+
+
 #파일 읽기
 f = open("demo.txt", "rt", encoding="utf-8")
 result = f.read()
 print(result)
+#처음으로 리셋
+f.seek(0)
+print(f.readline(), end="")
+print(f.readline(), end="")
+
+f.seek(0)
+lst = f.readline()
+for item in lst:
+    print(item)
+
+
 f.close()
 
 
