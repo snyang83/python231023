b7a58f488bcb46de7361acdd7b6a193805d6c3ab
snyang83 <sn01.yang@gmail.com>
2023-10-26 16:28:55 +0900

    PyQt 연습2

--- web1.py
+++ web1.py
@@ -7,4 +7,18 @@ page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체 생성 
 soup = BeautifulSoup(page, "html.parser")
 #전체 보기 
-print(soup.prettify())
+# print(soup.prettify())
+# <p> 태그 전체를 검색
+# print(soup.find_all("p"))
+# 첫번째 <p> 만 검색 
+# print(soup.find("p"))
+# < p class="outer-text"> 형태만 검색
+# print(soup.find_all("p", class_= "outer-text"))
+# attrs를 사용(Attributes)
+# print(soup.find_all("p", attrs={"class":"outer-text"}))
+# 특정 태그만 지정할 경우 id속성
+# print(soup.find_all(id="first"))
+# 태그 내부의 컨텐츠를 가져오기: .text
+for tag in soup.find_all("p"):
+    title = tag.text.strip()
+    print(title)
